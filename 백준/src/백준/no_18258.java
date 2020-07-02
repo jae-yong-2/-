1f1b91ba8ed51804666e007ed0b9bcb63ad57280
leejeayoun <ljy8972@naver.com>
2020-07-02 16:38:26 +0900

    큐 배열 답지(시간초과)

--- 백준/src/백준/no_18258.java
+++ 백준/src/백준/no_18258.java
@@ -9,38 +9,40 @@ public class no_18258 {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		InputStreamReader inf = new InputStreamReader(System.in);
-		BufferedReader in = new BufferedReader(inf);
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		String read_line = in.readLine();
 		int Q_end = -1;
 		int Q_start = 0;
-		int Q[] = new int[Integer.parseInt(read_line)];
-
+		int size = 0;
+		String Q[] = new String[Integer.parseInt(read_line)];
 		for (int i = 0; i < Q.length; i++) {
 			read_line = in.readLine();
+			// push
 			if (read_line.charAt(1) == 'u') {
-				Q_end++;// push
 				StringTokenizer token = new StringTokenizer(read_line);
 				token.nextToken();
-				Q[Q_end] = Integer.parseInt(token.nextToken());
+				Q_end++;
+				size++;
+				Q[Q_end] = token.nextToken();
 			}
 			// pop
 			else if (read_line.equals("pop")) {
-				if (Q_end - Q_start == -1) {
+				if (size == 0) {
 					System.out.println(-1);
 				} else {
 					System.out.println(Q[Q_start]);
 					Q_start++;
+					size--;
 				}
 			}
 			// size
 			else if (read_line.equals("size")) {
-				System.out.println(Q_end - Q_start + 1);
+				System.out.println(size);
 			}
 			// empty
 			else if (read_line.equals("empty")) {
 
-				if (Q_end - Q_start == -1) {
+				if (size == 0) {
 					System.out.println(1);
 				} else {
 					System.out.println(0);
@@ -50,7 +52,7 @@ public class no_18258 {
 			// front
 			else if (read_line.equals("front")) {
 
-				if (Q_end - Q_start == -1) {
+				if (size == 0) {
 					System.out.println(-1);
 				} else {
 					System.out.println(Q[Q_start]);
@@ -59,7 +61,7 @@ public class no_18258 {
 			// back
 			else if (read_line.equals("back")) {
 
-				if (Q_end - Q_start == -1) {
+				if (size == 0) {
 					System.out.println(-1);
 				} else {
 					System.out.println(Q[Q_end]);
